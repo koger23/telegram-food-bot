538684039dde38244bb7eab9b994462777e7084c
Gergely Kocsis <koger23@gmail.com>
2023-08-18 22:03:31 +0200

    new wizard with calendar in first step, working on second and evaluate selected date

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import { Scenes, session, Telegraf } from "telegraf";
 import { Secrets } from "./secrets.js";
-import { wizard } from "./scenes.js";
+import { wizard, mealPlannerWizard } from "./scenes.js";
 
-const stage = new Scenes.Stage([wizard]);
 const bot = new Telegraf(Secrets.TELEGRAM_BOT_TOKEN);
+const stage = new Scenes.Stage([wizard, mealPlannerWizard(bot)]);
+
 bot.use(session());
 bot.use(stage.middleware());
+
 bot.command("id", (ctx) => {
   ctx.scene.enter("wizard");
 });
+bot.hears("*", (ctx) => ctx.reply("Hey there"));
+
+bot.command("start", (ctx) => {
+  const chatId = ctx.chat.id;
+  ctx.scene.enter('meal-planner-wizard', chatId);
+});
+
+bot.use((ctx, next) => {
+  const message = ctx?.message;
+  const userId = message?.from?.id;
+  const text = message?.text;
+  // Check if the message is a command (starts with a slash)
+  if (text && !text.startsWith("/")) {
+    // Process the message or perform any desired actions here
+    // For demonstration purposes, let's just echo the message back to the user
+    ctx.reply(`You said: ${text}`);
+  }
+  // Call the next middleware
+  return next();
+});
+
 bot.launch();
--- scenes.js
+++ scenes.js
@@ -1,4 +1,5 @@
 import { Scenes } from "telegraf";
+import NavCalendar from "telegram-inline-calendar";
 
 export const wizard = new Scenes.WizardScene(
   "wizard",
@@ -19,3 +20,50 @@ export const wizard = new Scenes.WizardScene(
     return ctx.scene.leave();
   }
 );
+
+let calendar = null;
+const calendarOptions = {
+  date_format: "YYYY-MM-DD",
+  language: "en",
+  bot_api: "telegraf",
+};
+export const mealPlannerWizard = (bot) =>
+  new Scenes.WizardScene(
+    "meal-planner-wizard",
+    (ctx) => {
+      console.log("STEP 1");
+      if (calendar == null) {
+        calendar = new NavCalendar(bot, calendarOptions);
+      }
+      calendar.startNavCalendar(ctx);
+
+      return ctx.wizard.next();
+    },
+    (ctx) => {
+      console.log("STEP 2");
+      if (!calendar) {
+        ctx.wizard.back();
+      } else if (
+        ctx.callbackQuery.message.message_id ==
+        calendar.chats.get(ctx.callbackQuery.message.chat.id)
+      ) {
+        let res = ctx?.callbackQuery?.data;
+        // ctx.wizard.state.data.selectedDate = ctx.callbackQuery.data;
+        if (res != null && res !== -1) {
+          ctx.reply("You selected: " + res);
+          console.log(calendar.clickButtonCalendar(res));
+          ctx.wizard.state.data.name = res;
+
+          return ctx.wizard.next();
+        }
+      } else {
+        ctx.wizard.back();
+      }
+    },
+    (ctx) => {
+      ctx.wizard.state.data.phone = ctx.message.text;
+      ctx.reply(`Selected date is ${ctx.wizard.state.data.selectedDate}`);
+      ctx.reply(`Your phone is ${ctx.wizard.state.data.phone}`);
+      return ctx.scene.leave();
+    }
+  );
