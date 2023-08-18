0685850f8401b33d7366f994766e5338565668e6
Gergely Kocsis <koger23@gmail.com>
2023-08-18 20:17:42 +0200

    working example

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-import { Composer, Scenes, Markup, session, Telegraf } from "telegraf";
-import { TELEGRAM_BOT_TOKEN } from "./secrets.js";
+import { Scenes, session, Telegraf } from "telegraf";
+import { Secrets } from "./secrets.js";
+import { wizard } from "./scenes.js";
 
-const stepHandler = new Composer();
-stepHandler.action("next", (ctx) => {
-  ctx.reply("Step 2. Via inline button");
-  return ctx.wizard.next();
-});
-stepHandler.command("next", (ctx) => {
-  ctx.reply("Step 2. Via command");
-  return ctx.wizard.next();
-});
-stepHandler.use((ctx) =>
-  ctx.replyWithMarkdown("Press `Next` button or type /next")
-);
-
-const superWizard = new Scenes.WizardScene(
-  "super-wizard",
-  (ctx) => {
-    ctx.reply(
-      "Step 1",
-      Markup.inlineKeyboard([
-        Markup.urlButton("❤️", "http://telegraf.js.org"),
-        Markup.callbackButton("➡️ Next", "next"),
-      ]).extra()
-    );
-    return ctx.wizard.next();
-  },
-  stepHandler,
-  (ctx) => {
-    ctx.reply("Step 3");
-    return ctx.wizard.next();
-  },
-  (ctx) => {
-    ctx.reply("Step 4");
-    return ctx.wizard.next();
-  },
-  (ctx) => {
-    ctx.reply("Done");
-    return ctx.scene.leave();
-  }
-);
-
-const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
-const stage = new Scenes.Stage([superWizard], { default: "super-wizard" });
+const stage = new Scenes.Stage([wizard]);
+const bot = new Telegraf(Secrets.TELEGRAM_BOT_TOKEN);
 bot.use(session());
 bot.use(stage.middleware());
-bot.command('/id', ctx => {
-  ctx.scene.enter('super-wizard');
+bot.command("id", (ctx) => {
+  ctx.scene.enter("wizard");
 });
 bot.launch();
--- scenes.js
+++ scenes.js
@@ -1,32 +1,21 @@
 import { Scenes } from "telegraf";
-import { Stage } from "telegraf/typings/scenes";
 
-const contactDataWizard = new Scenes.WizardScene(
-  "CONTACT_DATA_WIZARD_SCENE_ID", // first argument is Scene_ID, same as for BaseScene
+export const wizard = new Scenes.WizardScene(
+  "wizard",
   (ctx) => {
-    ctx.reply("What is your name?");
-    ctx.wizard.state.contactData = {};
+    ctx.reply("What's your name?");
+    ctx.wizard.state.data = {};
     return ctx.wizard.next();
   },
   (ctx) => {
-    // validation example
-    if (ctx.message.text.length < 2) {
-      ctx.reply("Please enter name for real");
-      return;
-    }
-    ctx.wizard.state.contactData.fio = ctx.message.text;
-    ctx.reply("Enter your e-mail");
+    ctx.wizard.state.data.name = ctx.message.text;
+    ctx.reply("Enter your phone number");
     return ctx.wizard.next();
   },
-  async (ctx) => {
-    ctx.wizard.state.contactData.email = ctx.message.text;
-    ctx.reply("Thank you for your replies, we'll contact your soon");
-    await mySendContactDataMomentBeforeErase(ctx.wizard.state.contactData);
+  (ctx) => {
+    ctx.wizard.state.data.phone = ctx.message.text;
+    ctx.reply(`Your name is ${ctx.wizard.state.data.name}`);
+    ctx.reply(`Your phone is ${ctx.wizard.state.data.phone}`);
     return ctx.scene.leave();
   }
 );
-
-const stage = new Stage([contactDataWizard]);
-
-bot.use(session());
-bot.use(stage.middleware());
